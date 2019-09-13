9fa41ba195700c7efb2f2997d89827d71e1f1b55
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-09-13 21:54:35 +0900

    Add P30 AnsCandidate

--- Problem30.cpp
+++ Problem30.cpp
@@ -14,10 +14,14 @@ namespace prob30{
     std::vector<unsigned> num_box;
   };
   bool is_answer(const struct Num_Data& t) {
-    //std::assert(num_box.size() == 4);
+    //std::assert(t.num_box.size() == 4);
     if (is_in_Range(t.number) == false) {
       return false;
     } else {
+      std::cout << t.num_box[0] << ' '<< t.num_box[1] << ' '<< t.num_box[2] << ' '<< t.num_box[3] << '\n';
+      std::cout << "t.sum:" << t.number << '\n';
+
+
       std::vector<unsigned> number_vec(4);
       number_vec[0] = t.number/1000;
       number_vec[1] = (t.number/100)%10;
@@ -35,22 +39,41 @@ namespace prob30{
   std::vector<unsigned> answer() {
     std::vector<unsigned> number(10);
     for (auto i = 0;i < 10;++i) {
-      number[i] = std::pow(i, 5);
+      number[i] = std::pow(i, 4);
+      std::cout << number[i] << '\n';
     }
-    std::vector<unsigned> ans;
+    std::vector<struct Num_Data> ans_candidate;
     for (auto i = 0;i < 10;++i) {
-      for (auto j = 0;j < 4;++j) {
+      std::vector<unsigned> num_box_(4);
+      num_box_[0] = i;
+      for (auto j = i;j < 10;++j) {
+        num_box_[1] = j;
+        for (auto k = j;k < 10;++k) {
+          num_box_[2] = k;
+          for (auto l = k;l < 10;++l) {
+            num_box_[3] = l;
+            unsigned sum = number[i] + number[j] + number[k] + number[l];
+            struct Num_Data t;
+            t.num_box = num_box_;
+            t.number = sum;
+            if (is_answer(t)) {
+              ans_candidate.push_back(t);
+            }
+          }
+        }
       }
     }
+    std::cout << ans_candidate.size() << '\n';
     //Conbination
-    unsigned t_num = 1000;
-    std::vector<unsigned> t_num_box {1,0,0,0};
-    struct Num_Data t;
-    t.number = t_num;
-    t.num_box = t_num_box;
-    if (is_answer(t) == true) {
-      ans.push_back(t_num);
-    }
+    //unsigned t_num = 1000;
+    //std::vector<unsigned> t_num_box {1,0,0,0};
+    //struct Num_Data t;
+    //t.number = t_num;
+    //t.num_box = t_num_box;
+    std::vector<unsigned> ans;
+    //if (is_answer(t) == true) {
+    //  ans.push_back(t_num);
+    //}
     return ans;
   }
 }
